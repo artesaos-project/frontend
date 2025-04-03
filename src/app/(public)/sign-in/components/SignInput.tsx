f6800f21d3b18069f1fb24164e9275df37b61606
MARCUS VINICIUS DA CRUZ SANTOS <CG3028518@car.ifsp>
2025-04-02 21:55:49 -0300

    feat: Alinhando os componentes e terminando algumas estilizações

--- src/app/(public)/sign-in/components/SignInput.tsx
+++ src/app/(public)/sign-in/components/SignInput.tsx
@@ -6,7 +6,7 @@ interface SignInputProps{
 
 function SignInput(props: SignInputProps){
   return(
-    <Input className="bg-[#E4F5E9] w-[265px] h-[38px] rounded-3xl ml-12 px-5 py-[15px] mt-5 placeholder-[#859E8B] shadow-inner shadow-neutral-500 font-semibold" placeholder={props.placeholder}/>
+    <Input className="bg-[#E4F5E9] w-[265px] h-[38px] rounded-3xl ml-12 px-5 py-[15px] mt-5 placeholder:text-[#859E80] shadow-inner shadow-neutral-500 font-semibold" placeholder={props.placeholder}/>
   )
 }
 
--- src/app/(public)/sign-in/page.tsx
+++ src/app/(public)/sign-in/page.tsx
@@ -7,12 +7,12 @@ import SignInput from "./components/SignInput";
 
 function page(){
   return(
-    <div className="flex flex-col w-[393px] m-auto">
+    <div className="flex flex-col w-[393px] m-auto justify-center">
       <header className="w-full h-36">
         <Link href="/initial" className="pointer"><FiChevronLeft className="m-12" size={24}/></Link>
       </header>
       <div className="w-full">
-        <h1 className="font-bold text-[38px] ml-12">Olá!</h1>
+        <h1 className="font-bold text-[45px] ml-12">Olá!</h1>
         <p className="italic text-[15px] ml-12">Bom te ver de novo!</p>
         <SignInput placeholder={"email"}/>
         <SignInput placeholder={"senha"}/>
@@ -20,7 +20,10 @@ function page(){
       <div className="w-[305px] flex justify-end italic">
         <Link href="/#" className=" pointer underline text-right mt-7">Esqueceu sua senha?</Link>
       </div>
-      <Button className="bg-[#E05D00] w-44 h-10 rounded-xl">Continuar</Button>
+      <div className="w-full flex justify-center">
+        <Button className="bg-[#E05D00] w-44 h-10 rounded-xl inset-shadow-border shadow-neutral-800 inset-shadow-xl">Continuar</Button>
+      </div>
+      
     </div>
     
   );
