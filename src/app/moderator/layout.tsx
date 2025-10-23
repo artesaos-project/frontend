6a54221f0ecda43f1f7206ab2ec6018b26ddcad4
Daniel Morais <daniel.patrickmorais@gmail.com>
2025-10-22 23:33:45 -0300

    Revert "."
    This reverts commit 188afc6733f34c56bab01add2d87565e988083f1.

--- src/app/moderator/layout.tsx
+++ src/app/moderator/layout.tsx
@@ -1,16 +1,44 @@
 import ModeratorHeader from '@/components/features/moderator/moderator-header';
 import Footer from '@/components/footer';
+<<<<<<< Updated upstream
+import '../globals.css';
+
+export default function RootLayout({
+=======
+<<<<<<< HEAD
 
 export default function ModeratorLayout({
+=======
+import '../globals.css';
+
+export default function RootLayout({
+>>>>>>> afaa67f9ab8f5a1670aaf9d60a9a9f6e08f23968
+>>>>>>> Stashed changes
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
+<<<<<<< Updated upstream
+    <div>
+      <ModeratorHeader />
+      {children}
+      <Footer newsSubscription={false} />
+    </div>
+=======
+<<<<<<< HEAD
     <>
       <ModeratorHeader />
       {children}
       <Footer newsSubscription={false} />
     </>
+=======
+    <div>
+      <ModeratorHeader />
+      {children}
+      <Footer newsSubscription={false} />
+    </div>
+>>>>>>> afaa67f9ab8f5a1670aaf9d60a9a9f6e08f23968
+>>>>>>> Stashed changes
   );
 }
